874266131d89973c8d73994d323410abf59ecaf7
isaacbarcroft <i_barcroft@yahoo.com>
2021-09-27 14:22:04 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+
+
 function App() {
+
+
+  let [likes, setLikes] = useState(0);
+  
+  function updateLikes(){
+    setLikes(likes +1);
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+  
+    
+  <button onClick={updateLikes} type="buttton">{`${likes} ${(likes === 1) ? 'Like': 'Likes'}`}</button>
+  
+      </>
   );
 }
 
